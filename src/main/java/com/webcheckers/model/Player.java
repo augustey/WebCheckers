841c0686989b9dc6e6daf52bc7df9fbb217d8478
Jim <james@dbej.net>
2021-06-19 15:41:31 -0400

    Player now holds a String for name and has an equals and a hashcode method

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -1,6 +1,37 @@
 package com.webcheckers.model;
 
+import java.util.Objects;
+
+/**
+ * Value class, holds the unique name for the player
+ *
+ * @author <a href='mailto:jrl9984@rit.edu'>Jim Logan</a>
+ */
 public class Player
 {
+    private final String name; //name of the player will not change
 
+    /**
+     * Creates a new player data entity using a unique string
+     * @param name
+     */
+    public Player(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (!(o instanceof Player)) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(name);
+    }
 }
