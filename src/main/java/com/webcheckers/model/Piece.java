9786aab3b4d2b8f4cef8c72e50ac791ff82d0dc8
whd8254 <whd8254@g.rit.edu>
2021-06-19 21:08:04 -0400

    added comments to Piece

--- src/main/java/com/webcheckers/model/Piece.java
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -8,15 +8,28 @@ public class Piece {
     private Type type;
     private Color color;
 
+    /**
+     * constructor
+     * @param type
+     * @param color
+     */
     public Piece(Type type, Color color){
         this.type = type;
         this.color = color;
     }
 
+    /**
+     * returns the type of piece it is (king or regular)
+     * @return
+     */
     public Type getType (){
         return type;
     }
 
+    /**
+     * returns the color of the piece (red or white)
+     * @return
+     */
     public Color getColor(){
         return color;
     }
--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -1,4 +1,6 @@
 package com.webcheckers.model;
 
 public class Space {
+
+    private int cellIdx;//the cells in a row 0 - 7
 }
