ab0872fdd51b00960f6a8cdaed806bd19c7d755a
whd8254 <whd8254@g.rit.edu>
2021-06-19 21:04:50 -0400

    got the Piece class done

--- src/main/java/com/webcheckers/model/Piece.java
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -1,4 +1,24 @@
 package com.webcheckers.model;
 
 public class Piece {
+
+    public enum Type{SINGLE, KING};
+    public enum Color{RED, WHITE};
+
+    private Type type;
+    private Color color;
+
+    public Piece(Type type, Color color){
+        this.type = type;
+        this.color = color;
+    }
+
+    public Type getType (){
+        return type;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
 }
