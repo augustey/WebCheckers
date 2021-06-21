aa1795bdcc250d9ba55bb0f183dcfe462a8e6a58
yaa6681 <yaa6681@rit.edu>
2021-06-21 17:19:45 -0400

    Add some handle logic.

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -1,6 +1,8 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.application.GameCenter;
 import com.webcheckers.application.PlayerLobby;
+import com.webcheckers.application.PlayerService;
 import com.webcheckers.model.Player;
 import spark.*;
 
@@ -8,6 +10,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
+
+import static spark.Spark.halt;
 
 /**
  * UI controller to get the game route.
@@ -17,16 +21,21 @@ public class GetGameRoute implements Route {
     private static final Logger LOG = Logger.getLogger(GetGameRoute.class.getName());
     private final TemplateEngine templateEngine;
     private final PlayerLobby playerLobby;
+    private final GameCenter gameCenter;
 
     //Used to send opponents name when selected in the list
     private final String OPPONENT_PARAM = "opponent";
+
+    //Used to store and access player service in session object
+    public final String PLAYERSERVICE_ATTR = "PlayerService";
 
     /**
      * Constructor for GetGameRoute. Used to handle requests sent to "/game".
      * @param templateEngine Template engine used to render views.
      */
-    public GetGameRoute(final PlayerLobby playerLobby, final TemplateEngine templateEngine) {
+    public GetGameRoute(final PlayerLobby playerLobby, final GameCenter gameCenter, final TemplateEngine templateEngine) {
         this.playerLobby = Objects.requireNonNull(playerLobby, "playerLobby is required");
+        this.gameCenter = Objects.requireNonNull(gameCenter, "gameCenter is required");
         this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
         LOG.config("GetGameRoute is initialized.");
     }
@@ -48,9 +57,36 @@ public class GetGameRoute implements Route {
         //If player object does not exist, redirect them to the sign in page.
         if(player == null) {
             response.redirect(WebServer.SIGNIN_URL);
+            halt();
+            return null;
         }
 
-        //Player opponent =
+        //Attempt to retrieve playerService object
+        PlayerService playerService = httpSession.attribute(PLAYERSERVICE_ATTR);
+
+        //If player service does not exist, attempt to create new game with opponent
+        if(playerService == null) {
+            //Get the name of the opponent selected from the online list
+            String opponentName = request.queryParams(OPPONENT_PARAM);
+
+            //Retrieve the corresponding player object
+            Player opponent = playerLobby.getPlayer(opponentName);
+
+            //If the opponent is in the game then redirect them home
+            //with an error message
+            //TODO: Add error message functionality
+            if (opponent == null || gameCenter.isInGame(opponent)) {
+                response.redirect(WebServer.HOME_URL);
+                halt();
+                return null;
+            }
+
+            //Create a new game an get a player service for it
+            playerService = gameCenter.requestNewGame(player, opponent);
+
+            httpSession.attribute(PLAYERSERVICE_ATTR, playerService);
+        }
+
         Map<String, Object> vm = new HashMap<>();
 
         response.redirect(WebServer.HOME_URL);//templateEngine.render(new ModelAndView(vm , "game.ftl"));
