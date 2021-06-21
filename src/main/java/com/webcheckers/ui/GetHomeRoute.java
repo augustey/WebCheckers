c2102951411adc99c8e574704bcb076a33633d8c
yaa6681 <yaa6681@rit.edu>
2021-06-21 17:32:24 -0400

    Add refresh logic for GetHomeRoute

--- src/main/java/com/webcheckers/ui/GetHomeRoute.java
+++ src/main/java/com/webcheckers/ui/GetHomeRoute.java
@@ -5,11 +5,15 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
 
+import com.webcheckers.application.GameCenter;
 import com.webcheckers.application.PlayerLobby;
+import com.webcheckers.application.PlayerService;
 import com.webcheckers.model.Player;
 import spark.*;
 
 import com.webcheckers.util.Message;
+
+import static spark.Spark.halt;
 
 /**
  * The UI Controller to GET the Home page.
@@ -28,6 +32,7 @@ public class GetHomeRoute implements Route {
 
   private final PlayerLobby playerLobby;
   private final TemplateEngine templateEngine;
+  private final GameCenter gameCenter;
 
   /**
    * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
@@ -35,8 +40,9 @@ public class GetHomeRoute implements Route {
    * @param templateEngine
    *   the HTML template rendering engine
    */
-  public GetHomeRoute(final PlayerLobby playerLobby, final TemplateEngine templateEngine) {
+  public GetHomeRoute(final PlayerLobby playerLobby, final GameCenter gameCenter, final TemplateEngine templateEngine) {
     this.playerLobby = Objects.requireNonNull(playerLobby, "playerLobby is required");
+    this.gameCenter = Objects.requireNonNull(gameCenter, "gameCenter is required");
     this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
     //
     LOG.config("GetHomeRoute is initialized.");
@@ -66,6 +72,26 @@ public class GetHomeRoute implements Route {
       Player player = httpSession.attribute(PLAYER_KEY);
       vm.put("currentUser", player);
       vm.put("playerSet", playerLobby.getPlayerSet());
+
+      //Attempt to get player service object
+      PlayerService playerService = httpSession.attribute(GetGameRoute.PLAYER_SERVICE_ATTR);
+
+      //Check if the player is in a game
+      //If so then redirect them to their game page
+      if(gameCenter.isInGame(player)) {
+        //Check if their player service attribute is defined
+        //If not this means another user requested a game with them
+        //and needs to have a service for them created
+        if(playerService != null) {
+          playerService = gameCenter.getPlayerService(player);
+          httpSession.attribute(GetGameRoute.PLAYER_SERVICE_ATTR, playerService);
+        }
+
+        //Redirect the player to the game page
+        response.redirect(WebServer.GAME_URL);
+        halt();
+        return null;
+      }
     }
 
     int count = playerLobby.getPlayerSet().size();
