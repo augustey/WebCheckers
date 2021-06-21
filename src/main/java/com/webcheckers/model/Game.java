1799d650357ea3bb62f0c33250737e888e11ce95
yaa6681 <yaa6681@rit.edu>
2021-06-21 17:18:21 -0400

    Add Game class.

---
+++ src/main/java/com/webcheckers/model/Game.java
@@ -0,0 +1,40 @@
+package com.webcheckers.model;
+
+/**
+ * Game model class containing the logic of a standard game of Checkers
+ * @author Yaqim Auguste (yaa6681@rit.edu)
+ */
+public class Game {
+    private final Player redPlayer;
+    private final Player whitePlayer;
+
+    /*
+    TODO: Add board class
+     */
+
+    /**
+     * Constructor for game.
+     * @param redPlayer the red player who starts first
+     * @param whitePlayer the white player who goes second
+     */
+    public Game(Player redPlayer, Player whitePlayer) {
+        this.redPlayer = redPlayer;
+        this.whitePlayer = whitePlayer;
+    }
+
+    /**
+     * Getter for redPlayer
+     * @return whitePlayer
+     */
+    public Player getRedPlayer() {
+        return redPlayer;
+    }
+
+    /**
+     * getter for whitePlayer
+     * @return whitePlayer
+     */
+    public Player getWhitePlayer() {
+        return whitePlayer;
+    }
+}
