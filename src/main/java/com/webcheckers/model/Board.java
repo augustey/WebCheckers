f3a051bec5fcdd40ceb461749be8fa60fc9f8e02
whd8254 <whd8254@g.rit.edu>
2021-06-21 10:15:21 -0400

    commented the new board constructor

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -6,9 +6,7 @@ import java.util.Iterator;
 public class Board implements Iterable<Row>{
 
     private final int BOARD_DIM = 8;//the size of the board
-//    private ArrayList<Row> board;
-//    private Piece.Color m
-//    private Space [][] board;
+
     private ArrayList<Row> board = new ArrayList<Row>();
 
     @Override
@@ -21,34 +19,30 @@ public class Board implements Iterable<Row>{
      */
     public Board(){
         Space space;
-//        Row curRow;
-//        ArrayList<Row> board = new ArrayList<Row>();
-//        board = new Space[BOARD_DIM][BOARD_DIM];
         for(int row = 0; row < BOARD_DIM; row++){
-            //create new row
-            ArrayList<Space> curRow = new ArrayList<Space>();
-//            curRow = new Row()
+            ArrayList<Space> curRow = new ArrayList<Space>();//holds what will latter be put into a row object
             for(int col = 0; col < BOARD_DIM; col++){
-                space = new Space(col);
+                space = new Space(col);//creates a blank space
                 if(col % 2 + row % 2 == 1){//is on red if only row or col is even but not both
-                    if(row > BOARD_DIM - 2)//for white piece placement
+                    if(row > BOARD_DIM - 3)//for white piece placement
                     {
-                        space.setPiece(new Piece(Piece.Type.SINGLE, Piece.Color.WHITE));
+                        space.setPiece(new Piece(Piece.Type.SINGLE, Piece.Color.WHITE));//fills the blank space with a white piece
                     }
                     else if(row < 3){//for red piece placement
-                        space.setPiece(new Piece(Piece.Type.SINGLE, Piece.Color.RED));
+                        space.setPiece(new Piece(Piece.Type.SINGLE, Piece.Color.RED));//fills the blank space with a red piece
                     }
-                    curRow.add(space);//building up array list that will be made in to row element latter
+                    curRow.add(space);//puts space into the collection holding all spaces in row
                 }
-                this.board.add(new Row(row, curRow));//adding row to board
+
             }
+            this.board.add(new Row(row, curRow));//creates a new row and adds it to the board
         }
         System.out.println(board);
-
     }
 
     public static void main(String[] args) {//for debugging purposes only
         new Board();
+//        System.out.println(this.board);
 
     }
 }
