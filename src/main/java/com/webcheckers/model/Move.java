d76ca746a581ccf94e3bd9380b183fbc966c5e86
whd8254 <whd8254@g.rit.edu>
2021-06-21 22:21:36 -0400

    worked on Move class

--- src/main/java/com/webcheckers/model/Move.java
+++ src/main/java/com/webcheckers/model/Move.java
@@ -4,4 +4,31 @@ package com.webcheckers.model;
  * this class handles moves
  */
 public class Move {
+    private final Space start;
+    private final Space end;
+    public enum TypeOfMove{SINGLE_MOVE, JUMP};
+
+    private TypeOfMove typeOfMove;
+
+
+    public Move(Space start, Space end) {
+        this.start = start;
+        this.end = end;
+        isJump();
+        isSingleMove();
+    }
+    public void isJump(){
+
+    }
+    public void isSingleMove(){
+
+    }
+    public TypeOfMove getTypeOfMove(){
+        return typeOfMove;
+    }
+
+    //check if land is empty
+    //check if move
+    //check if jump
+        //check if the jumped is correct
 }
