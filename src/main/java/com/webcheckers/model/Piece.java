4db22ef2acef4600292e8e26efc764061a4208de
nmr3095 <nmr3095@rit.edu>
2021-06-25 16:59:39 -0400

    Added documentation to the Piece class.

--- src/main/java/com/webcheckers/model/Piece.java
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -1,39 +1,51 @@
 package com.webcheckers.model;
 
 public class Piece {
-
-
-
+    /**
+     * Enum for the type of pieces.
+     */
     public enum Type{SINGLE, KING};
+    /**
+     * Enum for the color of pieces.
+     */
     public enum Color{RED, WHITE};
 
+    // Type of piece
     private Type type;
+    // Color of piece
     private Color color;
 
     /**
-     * constructor
+     * Constructor for a chess piece.
+     *
      * @param type
+     *     The type of piece.
+     *
      * @param color
+     *     The color of the piece.
      */
-    public Piece(Type type, Color color){
+    public Piece(Type type, Color color) {
         this.type = type;
         this.color = color;
     }
 
     /**
-     * returns the type of piece it is (king or regular)
+     * A getter method for the type of piece.
+     *
      * @return
+     *     The type of piece.
      */
-    public Type getType (){
+    public Type getType () {
         return type;
     }
 
     /**
-     * returns the color of the piece (red or white)
+     * A getter method for the color of piece.
+     *
      * @return
+     *     The color of piece.
      */
-    public Color getColor(){
+    public Color getColor() {
         return color;
     }
-
 }
