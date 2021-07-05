571fab885f3463d883577081f980a63f3fb6b36c
nmr3095 <nmr3095@rit.edu>
2021-07-04 21:17:05 -0400

    Finished and documentated the unit test suite for Space (a model-tier component).

--- src/test/java/com/webcheckers/ui/model/SpaceTest.java
+++ src/test/java/com/webcheckers/ui/model/SpaceTest.java
@@ -21,19 +21,15 @@ import com.webcheckers.model.Space;
 @Tag("Model-tier")
 
 public class SpaceTest {
-    private static final int ROW_VALUE = 3;
+    private static final int ROW_VALUE = 2;
     private static final int COL_VALUE = 5;
     private Piece piece;
-    private static final boolean IS_VALID = false;
-
+    private static final boolean IS_VALID = true;
     private Space CuT;
 
     @BeforeEach
     public void testSetup() {
         piece = mock(Piece.class);
-        when(piece.getColor()).thenReturn(Piece.Color.RED);
-        when(piece.getType()).thenReturn(Piece.Type.SINGLE);
-
         CuT = new Space(ROW_VALUE, COL_VALUE, null, IS_VALID);
     }
 
@@ -59,5 +55,42 @@ public class SpaceTest {
     @Test
     public void ctor_get_col_test() {
         assertEquals(COL_VALUE, CuT.getColIdx());
+    }
+
+    /**
+     * Test the piece getter in Space where the piece is null.
+     */
+    @Test
+    public void ctor_get_piece_test() {
+        assertNull(CuT.getPiece());
+    }
+
+    /**
+     * Test the piece setter in Space
+     */
+    @Test
+    public void ctor_set_piece_test() {
+        CuT.setPiece(piece);
+        assertEquals(piece, CuT.getPiece());
+    }
+
+    /**
+     * Test the Space's validity.
+     */
+    @Test
+    public void ctor_space_validity_test() {
+        assertTrue(CuT.isValid());
+        CuT.setPiece(piece);
+        assertFalse(CuT.isValid());
+    }
+
+    /**
+     * Test the Space if it has a piece.
+     */
+    @Test
+    public void ctor_space_has_piece_test() {
+        assertFalse(CuT.hasPiece());
+        CuT.setPiece(piece);
+        assertTrue(CuT.hasPiece());
     }
 }
