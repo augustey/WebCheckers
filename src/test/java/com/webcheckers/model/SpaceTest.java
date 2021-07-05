1ab419957f17f4dafbcd7281df0498f85294352a
nmr3095 <nmr3095@rit.edu>
2021-07-05 18:47:51 -0400

    Added documentation to both SpaceTest and PostSignInRouteTest. Added another test to PostSignInRouteTest.

--- src/test/java/com/webcheckers/model/SpaceTest.java
+++ src/test/java/com/webcheckers/model/SpaceTest.java
@@ -36,8 +36,10 @@ public class SpaceTest {
      */
     @BeforeEach
     public void testSetup() {
+        // Mock object for Piece.
         piece = mock(Piece.class);
 
+        // Creating the CuT.
         CuT = new Space(ROW_VALUE, COL_VALUE, null, IS_VALID);
     }
 
@@ -71,6 +73,7 @@ public class SpaceTest {
     @Test
     public void ctor_get_piece_test() {
         assertNull(CuT.getPiece());
+        // Setting the piece to null to test that the piece is actually placed.
         CuT.setPiece(piece);
         assertNotNull(CuT.getPiece());
     }
@@ -81,15 +84,18 @@ public class SpaceTest {
     @Test
     public void ctor_set_piece_test() {
         CuT.setPiece(piece);
+        // Testing that piece set is the same piece created.
         assertEquals(piece, CuT.getPiece());
     }
 
     /**
-     * Test the Space's validity.
+     * Test the Space's validity for placing a piece.
      */
     @Test
     public void ctor_space_validity_test() {
+        // Testing that space is empty and a black/true space.
         assertTrue(CuT.isValid());
+        // Testing that space is now filled and no longer valid.
         CuT.setPiece(piece);
         assertFalse(CuT.isValid());
     }
@@ -99,7 +105,9 @@ public class SpaceTest {
      */
     @Test
     public void ctor_space_has_piece_test() {
+        // Testing if the space recognizes having a piece on it.
         assertFalse(CuT.hasPiece());
+        // Setting a piece to test the space recognizing a piece on it.
         CuT.setPiece(piece);
         assertTrue(CuT.hasPiece());
     }
