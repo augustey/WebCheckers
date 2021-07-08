b0281f19aa44fbefd509420b865203ee6aa687f8
Jim <james@dbej.net>
2021-07-08 10:50:29 -0400

    completed resignGameRoute

--- src/main/java/com/webcheckers/ui/PostResignGameRoute.java
+++ src/main/java/com/webcheckers/ui/PostResignGameRoute.java
@@ -2,7 +2,10 @@ package com.webcheckers.ui;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.webcheckers.application.GameCenter;
+import com.webcheckers.application.GameWin;
 import com.webcheckers.application.PlayerService;
+import com.webcheckers.model.Game;
 import com.webcheckers.util.Message;
 import spark.*;
 
@@ -11,10 +14,12 @@ import java.util.Map;
 
 public class PostResignGameRoute implements Route
 {
+    private GameCenter gameCenter;
     private PlayerService playerService;
 
-    public PostResignGameRoute()
+    public PostResignGameRoute(final GameCenter gameCenter)
     {
+        this.gameCenter = gameCenter;
     }
 
 
@@ -22,17 +27,20 @@ public class PostResignGameRoute implements Route
     public Object handle(Request request, Response response)
     {
         final Session httpSession = request.session();
-        final Map<String, Object> modeOp = new HashMap<>();
         Gson gson = new GsonBuilder().create();
 
         playerService = httpSession.attribute(GetGameRoute.PLAYER_SERVICE_KEY);
+        Game game = playerService.getGame();
+        GameWin gameWin = new GameWin(gameCenter, game);
 
         String gameOverMessage = playerService.getPlayer() + " has resigned.";
-
-        //trigger game over
+        if(gameWin.triggerGameOver(gameOverMessage))
+        {
+            Message message;
+            message = Message.info(gameOverMessage);
+        }
 
         Message message;
-
         message = Message.info(gameOverMessage);
 
         return gson.toJson(message);
