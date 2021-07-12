584cc9ab410ba046edada426496a60f8f65e5b19
Jim <james@dbej.net>
2021-07-12 13:32:24 -0400

    completed test for GameWin

--- src/test/java/com/webcheckers/application/GameWinTest.java
+++ src/test/java/com/webcheckers/application/GameWinTest.java
@@ -1,7 +1,6 @@
 package com.webcheckers.application;
 
-import com.webcheckers.model.Game;
-import com.webcheckers.model.Player;
+import com.webcheckers.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -53,34 +52,149 @@ public class GameWinTest
         assertFalse(game.isGameOver());
     }
 
-//    /**
-//     * Test for getGameMessage
-//     */
-//    @Test
-//    public void test_getGameOverMessage()
-//    {
-//        String expected = GameWin.NOT_OVER;
-//
-//        String actual = CuT.getGameOverMessage();
-//
-//        assertEquals(expected, actual);
-//    }
+    /**
+     * Test for getGameMessage
+     */
+    @Test
+    public void test_getGameOverMessage()
+    {
+        String actual = CuT.getGameOverMessage();
 
-//    /**
-//     * Test to trigger an end to a game
-//     */
-//    @Test
-//    public void test_triggerGameOver()
-//    {
-//        String expected = GameWin.NO_PIECES;
-//
-//        boolean result = CuT.triggerGameOver(GameWin.NO_PIECES);
-//
-//        assertTrue(game.isGameOver());
-//        assertTrue(CuT.isGameOver());
-//        assertEquals(expected, CuT.getGameOverMessage());
-//        assertTrue(result);
-//    }
+        assertNull(actual);
+    }
+
+    /**
+     * Test to trigger an end to a game
+     */
+    @Test
+    public void test_triggerGameOver()
+    {
+        String expected = "over";
+
+        boolean result = CuT.triggerGameOver("over");
+
+        assertTrue(game.isGameOver());
+        assertTrue(CuT.isGameOver());
+        assertEquals(expected, CuT.getGameOverMessage());
+        assertTrue(result);
+    }
+
+    /**
+     * Test for checkPieceCount
+     */
+    @Test
+    public void test_checkPieceCount()
+    {
+        int expected = 12;
+
+        int actual = CuT.checkPieceCount(game.getBoard(), Piece.Color.RED);
+
+        assertSame(expected, actual);
+    }
+
+    /**
+     * test for if checkPieceGameOver is false
+     */
+    @Test
+    public void test_checkPieceGameOver_false()
+    {
+        assertFalse(CuT.checkPieceGameOver(game.getBoard(), Piece.Color.RED));
+    }
+
+    /**
+     * Test for checkPieceGameOver if the player is red
+     */
+    @Test
+    public void test_checkPieceGameOver_red()
+    {
+        String expected = "2 has captured all pieces!";
+        clearColor(game.getBoard(), Piece.Color.RED);
+
+        boolean result = CuT.checkPieceGameOver(game.getBoard(), Piece.Color.RED);
+        String actual = CuT.getGameOverMessage();
+
+        assertTrue(result);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test for checkPieceGameOver if the player is white
+     */
+    @Test
+    public void test_checkPieceGameOver_white()
+    {
+        String expected = "1 has captured all pieces!";
+        clearColor(game.getBoard(), Piece.Color.WHITE);
+
+        boolean result = CuT.checkPieceGameOver(game.getBoard(), Piece.Color.WHITE);
+        String actual = CuT.getGameOverMessage();
+
+        assertTrue(result);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test for checkBlockedGameOver if the player is red
+     */
+    @Test
+    public void test_checkBlockedGameOver_red()
+    {
+        String expected = "1 has all of their pieces blocked!";
+
+        boolean result = CuT.checkBlockedGameOver(Piece.Color.RED);
+        String actual = CuT.getGameOverMessage();
+
+        assertTrue(result);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test for checkBlockedGameOver if the player is white
+     */
+    @Test
+    public void test_checkBlockedGameOver_white()
+    {
+        String expected = "2 has all of their pieces blocked!";
+
+        boolean result = CuT.checkBlockedGameOver(Piece.Color.WHITE);
+        String actual = CuT.getGameOverMessage();
+
+        assertTrue(result);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test for checkResignGameOver
+     */
+    @Test
+    public void test_checkResignGameOver() {
+        String expected = "1 has resigned.";
+
+        boolean result = CuT.checkResignGameOver(player1);
+        String actual = CuT.getGameOverMessage();
+
+        assertTrue(result);
+        assertEquals(expected, actual);
+    }
 
 
+    /**
+     * Helper method to clear board of a color
+     * @param board
+     * @param color
+     */
+    private void clearColor(Board board, Piece.Color color) {
+        for(int i = 0; i < board.BOARD_DIM; i++) {
+            for(int j = 0; j < board.BOARD_DIM; j++) {
+                Piece piece = board.getBoard()[i][j].getPiece();
+                try {
+                    if(piece.getColor() == color) {
+                        board.getBoard()[i][j].setPiece(null);
+                    }
+                } catch (NullPointerException npe) {
+                    continue;
+                }
+            }
+        }
+    }
 }
