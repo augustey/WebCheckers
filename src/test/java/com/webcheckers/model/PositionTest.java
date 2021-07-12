ea26d9fee99e57b96c10358ef905024686898310
nmr3095 <nmr3095@rit.edu>
2021-07-12 13:59:59 -0400

    Removed SingleMovePosition as it is never used due to using SingleMove.

---
+++ src/test/java/com/webcheckers/model/PositionTest.java
@@ -0,0 +1,15 @@
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.Tag;
+
+/**
+ * The unit test suite for the {@link Position} component.
+ *
+ * @author <a href = 'mailto:nmr3095@rit.edu'>Neel Raj</a>
+ */
+
+@Tag("Model-tier")
+
+public class PositionTest {
+
+}
