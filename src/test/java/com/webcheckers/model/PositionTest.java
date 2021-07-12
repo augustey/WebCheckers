b0b4703b601df55dccf5f21e58aac35fc761506b
nmr3095 <nmr3095@rit.edu>
2021-07-12 15:05:22 -0400

    Finished unit test suite PositionTest.

--- src/test/java/com/webcheckers/model/PositionTest.java
+++ src/test/java/com/webcheckers/model/PositionTest.java
@@ -1,6 +1,10 @@
 package com.webcheckers.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The unit test suite for the {@link Position} component.
@@ -12,4 +16,56 @@ import org.junit.jupiter.api.Tag;
 
 public class PositionTest {
 
+    // Constants for testing.
+    private static final int ROW_VALUE = 2;
+    private static final int COL_VALUE = 5;
+
+    /**
+     * The component-under-test (CuT).
+     */
+    private Position CuT;
+
+    /**
+     * Setup the new mock object for the unit tests.
+     */
+    @BeforeEach
+    public void testSetup() {
+        // Creating the CuT.
+        CuT = new Position(ROW_VALUE, COL_VALUE);
+    }
+
+    /**
+     * Test that the Position constructor works without failing.
+     */
+    @Test
+    public void arg_test() {
+        new Position(1, 2);
+    }
+
+    /**
+     * Test the cell getter in Position.
+     */
+    @Test
+    public void get_cell_test() {
+        assertEquals(COL_VALUE, CuT.getCell());
+    }
+
+    /**
+     * Test the row getter in Position.
+     */
+    @Test
+    public void get_row_test() {
+        assertEquals(ROW_VALUE, CuT.getRow());
+    }
+
+    /**
+     * Test the Position equals method.
+     */
+    @Test
+    public void position_equals_test() {
+        assertTrue(CuT.equals(CuT));
+        // Test equals with a different position object
+        Position position = new Position(1, 2);
+        assertFalse(CuT.equals(position));
+    }
 }
