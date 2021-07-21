0c2d6999b3e69d1daa380510ff98d3815bf9d2bf
nmr3095 <nmr3095@rit.edu>
2021-07-21 18:40:23 -0400

    Changed Turn to follow proper OO principles. It no longer takes a board object.

--- src/main/java/com/webcheckers/model/Turn.java
+++ src/main/java/com/webcheckers/model/Turn.java
@@ -6,19 +6,19 @@ import java.util.ArrayList;
  * This class is for storing the moves that make up a turn
  *
  * @author <a href = 'mailto:whd8254@rit.edu'>William Dabney</a>
+ * @author <a href = 'mailto:nmr3095@rit.edu'>Neel Raj</a>
  */
 public class Turn {
     private ArrayList<Move> moves;
-    private Board board;
-//    private Board.MoveType moveType;
+    private final Board.MoveType moveType;
 
-    public Turn(Board board){
-        //TODO get move type
-        this.board = board;
-//        this.moveType = moveType;
-//        System.out.println("Turn constuctor: " + moveType);
-        this.moves = new ArrayList<Move>();
-
+    public Turn(Board.MoveType moveType){
+        switch (moveType) {
+            case Jump: this.moveType = Board.MoveType.Jump; break;
+            case Single: this.moveType = Board.MoveType.Single; break;
+            default: this.moveType = Board.MoveType.Blocked; break;
+        }
+        this.moves = new ArrayList<>();
     }
 
     /**
@@ -30,7 +30,7 @@ public class Turn {
 
 //        System.out.println("Turn moveType: " + moveType);
         //If a JumpMove is required then the move is converted to a JumpMove
-        if(board.getMoveType() == Board.MoveType.Jump) {
+        if(moveType == Board.MoveType.Jump) {
             JumpMove jumpMove = new JumpMove(move);
             //This uses the custom equals method in JumpMove to make sure that no piece is jumped twice
 
@@ -39,14 +39,11 @@ public class Turn {
                 moves.add(jumpMove);
 //                System.out.println(moves);
 //                System.out.println("Size: " + moves.size());
-
-
                 return true;
             }
-
         }
         //If a SingleMove is required then the move is converted to a SingleMove
-        else if(board.getMoveType()  == Board.MoveType.Single){
+        else if(moveType  == Board.MoveType.Single){
             System.out.println("got to single");
             //TODO create new Single Move
             SingleMove singleMove = new SingleMove(move);
@@ -59,10 +56,10 @@ public class Turn {
     /**
      * Removes the last move in the turn
      */
-        public Move removeMove() {
-            int i = moves.size() - 1;
-            return moves.remove(i);
-        }
+    public Move removeMove() {
+        int i = moves.size() - 1;
+        return moves.remove(i);
+    }
 
     /**
      * Getter method for collection of moves that make up the turn
@@ -71,8 +68,4 @@ public class Turn {
     public ArrayList<Move> getMoves() {
         return moves;
     }
-
 }
-
-
-
